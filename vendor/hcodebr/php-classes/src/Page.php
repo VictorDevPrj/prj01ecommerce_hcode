9f3f8b9e67052d6ff80aff8788bdc2a0afb27e39
Victor <victordevprj@gmail.com>
2020-07-19 16:25:09 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,10 +7,14 @@ class Page {
     private $tpl;
     private $options = [];
     private $default = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
     public function __construct($opts = array(), $tpl_dir = "/views/"){
+
+        //$this->default["data"]["session"] = $_SESSION;
 
         $this->options = array_merge($this->default, $opts);
 
@@ -26,7 +30,7 @@ class Page {
         
         $this->setData($this->options["data"] );
 
-        $this->tpl->draw("header");
+        if($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -45,7 +49,7 @@ class Page {
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 
 
